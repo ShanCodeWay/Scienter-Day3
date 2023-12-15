37f8027e591ca9763d11456eae63f8ed662ae215
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2023-12-15 10:42:57 +0530

    Create all imports to same file

---
+++ LogType/Src/Imports/Imports.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const MyAll= {
+    React,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    Text,
+    useColorScheme,
+    View,
+    StyleSheet,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+  };
+  
+  export default MyAll;
