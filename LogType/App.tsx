6e0faf64d697459471e352de933180b02c39653f
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2023-12-15 12:00:38 +0530

    convert some JavaScript to TypeScript

--- LogType/App.tsx
+++ LogType/App.tsx
@@ -1,63 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import MyAll from './Src/Imports/Imports';
-
-const 
-{ React, SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View, StyleSheet, Colors, DebugInstructions, Header, 
-LearnMoreLinks, ReloadInstructions } = MyAll;
-
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+import React from 'react';
+import {View,Text} from 'react-native';
+import Navigator from './Src/Navigator/navigator';
+const App: React.FC = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Navigator/>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
---
+++ LogType/Src/Screens/signupScreen.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {
+  NavigationContainer,
+  createStackNavigator,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  Image,
+  AppbarImage,
+  useState as useStateImport,
+  backgroundImage,
+  useNavigation,
+} from '../Imports/imports';
+
+import { signupScreenStyles } from '../Styles/signupScreenStyles';
+
+const SignupScreen: React.FC = () => {
+  const navigation = useNavigation();
+  const [username, setUsername] = useStateImport<string>('');
+  const [email, setEmail] = useStateImport<string>('');
+  const [password, setPassword] = useStateImport<string>('');
+
+  const handleSignup = () => {
+    try {
+      console.log('Username:', username);
+      console.log('Email:', email);
+      console.log('Password:', password);
+
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error during signup:', error);
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
+      <ImageBackground source={backgroundImage} style={signupScreenStyles.backgroundImage}>
+        <View style={signupScreenStyles.container as any} >
+          <TextInput
+            style={signupScreenStyles.input as any}
+            placeholder="Enter your name"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+          />
+          <TextInput
+            style={signupScreenStyles.input as any}
+            placeholder="Enter your Email"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+          />
+          <TextInput
+            style={signupScreenStyles.input as any}
+            placeholder="Enter your Password"
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+          <TextInput
+            style={signupScreenStyles.input as any}
+            placeholder="Confirm your Password"
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+
+          <TouchableOpacity style={signupScreenStyles.buttonS as any} onPress={handleSignup}>
+            <Text style={signupScreenStyles.buttonText as any}>Sign Up</Text>
+          </TouchableOpacity>
+          <Text style={signupScreenStyles.signup as any}>Already have an account?</Text>
+          <TouchableOpacity style={signupScreenStyles.buttonL as any} onPress={() => navigation.navigate('Log-in' as never ) }>
+            <Text style={signupScreenStyles.buttonText as any}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default SignupScreen;
